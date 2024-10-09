e9803a3c02bcae1f6214ceb366f2969908a31c1b
Samuel Nelson <samuel.nelson@jostle.me>
2024-10-09 14:38:45 -0700

    Added packages for doctor page

--- virtual-er/src/app/doctor/page.tsx
+++ virtual-er/src/app/doctor/page.tsx
@@ -1,7 +1,30 @@
+import {
+    Menubar,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarSeparator,
+    MenubarShortcut,
+    MenubarTrigger,
+  } from "@/components/ui/menubar"
 export default function Page() {
     return (
         <form className="space-y-3">
-            
+            <Menubar>
+            <MenubarMenu>
+                <MenubarTrigger>Doctor</MenubarTrigger>
+                <MenubarContent>
+                <MenubarItem>
+                    New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem>New Window</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>Share</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>Logout</MenubarItem>
+                </MenubarContent>
+            </MenubarMenu>
+            </Menubar>
         </form >
     )
 }
