4a0002ba58e3046cab976a34a61b4fe4b360e52e
Rafael Edora <rvedora@gmail.com>
2024-10-15 22:41:59 -0700

    Add header to login page

--- virtual-er/src/app/login/page.tsx
+++ virtual-er/src/app/login/page.tsx
@@ -1,14 +1,18 @@
 import LoginForm from "@/components/ui/login-form"
+import Header from "@/components/ui/header"
 
 export default function Page() {
     return (
-        <div className="grid gap-y-2 mt-6">
-            <div className="m-auto font-bold">
-                <h1 className="text-2xl">
-                    Log in to Virtual ER
-                </h1>
+        <div>
+            <Header title=""></Header>
+            <div className="grid gap-y-2 mt-6">
+                <div className="m-auto font-bold">
+                    <h1 className="text-2xl">
+                        Log in to Virtual ER
+                    </h1>
+                </div>
+                <LoginForm />
             </div>
-            <LoginForm />
         </div>
     )
 }
--- virtual-er/src/components/ui/header.tsx
+++ virtual-er/src/components/ui/header.tsx
@@ -1,29 +1,28 @@
 import {
-    Menubar,
-    MenubarContent,
-    MenubarItem,
-    MenubarMenu,
-    MenubarSeparator,
-    MenubarTrigger,
-  } from "@/components/ui/menubar";
-  import { Button } from "@/components/ui/button";
-  import Link from 'next/link';
-  
-  interface HeaderProps {
-    title: string; 
-  }
-  
-  export default function Header({ title }: HeaderProps) {
-    return (
-        <div className="flex items-center justify-between p-4 bg-gray-100">
-        <h1 className="text-xl font-bold">VirtualER</h1>
-        <div className="flex items-center space-x-2">
-          <span className="text-sm text-gray-600">Signed in as {title}</span> {/* With {title} we can display a specific user type for each page where the title bar is used. Ex: <Header title="Healthcare Professional"/> */}
-          <Link href="/login">
-               <Button variant="outline">Logout</Button>
-          </Link>
-        </div>
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { Button } from "@/components/ui/button";
+import Link from 'next/link';
+
+interface HeaderProps {
+  title: string;
+}
+
+export default function Header({ title }: HeaderProps) {
+  return (
+    <div className="flex items-center justify-between p-4 bg-gray-100">
+      <h1 className="text-xl font-bold">VirtualER</h1>
+      <div className="flex items-center space-x-2">
+        <span className="text-sm text-gray-600">{title != "" ? `Signed in as ${title}` : ""}</span> {/* With {title} we can display a specific user type for each page where the title bar is used. Ex: <Header title="Healthcare Professional"/> */}
+        <Link href="/login">
+          <Button variant="outline">{title == "" ? "Login" : "Logout"}</Button>
+        </Link>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
