a0e2c84b1a57009e7b5477f4fc0bad369041d62f
Rafael Edora <rvedora@gmail.com>
2024-11-04 23:16:49 -0800

    fix login page

--- virtual-er/src/app/login/page.tsx
+++ virtual-er/src/app/login/page.tsx
@@ -1,10 +1,10 @@
+import LoginForm from "@/components/ui/auth/login-form"
 import Header from "@/components/ui/header"
-import LoginForm from "@/components/ui/login-form"
 
 export default function Page() {
     return (
         <div>
-            <Header title=""></Header>
+            <Header />
             <div className="grid gap-y-2 mt-6">
                 <div className="m-auto font-bold">
                     <h1 className="text-2xl">
