457416ff648be15146503420c26bb4b59217c0c4
Rafael Edora <rvedora@gmail.com>
2024-11-05 11:58:44 -0800

    fix logout button

--- virtual-er/src/components/ui/auth/logoutButton.tsx
+++ virtual-er/src/components/ui/auth/logoutButton.tsx
@@ -5,6 +5,6 @@ import { signOut } from "next-auth/react";
 
 export default function LogoutButton() {
     return (
-        <Button onClick={() => signOut} variant="outline">Sign Out</Button>
+        <Button onClick={() => signOut()} variant="outline">Sign Out</Button>
     )
 }
