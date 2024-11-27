b0c71e01904bb2293157b7f6938269b812a1a7e7
Rafael Edora <rvedora@gmail.com>
2024-11-27 14:07:32 -0800

    fix hydration error

--- virtual-er/src/components/auth/roleMessage.tsx
+++ virtual-er/src/components/auth/roleMessage.tsx
@@ -1,11 +1,16 @@
 import { getRole } from "@/lib/actions";
 
+function titleCaseWord(word: string) {
+    if (!word) return word;
+    return word[0].toUpperCase() + word.slice(1).toLowerCase();
+}
+
 export default async function RoleMessage() {
     const role = await getRole();
 
     return (
-        <span className="text-sm text-gray-600">
-            <p>Signed in as <span className="capitalize">{role}</span></p>
-        </span>
+        <div className="text-sm text-gray-600">
+            <p>{`Signed in as ${titleCaseWord(role)}`}</p>
+        </div>
     )
 }
