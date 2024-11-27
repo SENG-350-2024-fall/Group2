d0c51fb9e90a656c452037daa86844afb189d6aa
Rafael Edora <rvedora@gmail.com>
2024-11-26 20:34:04 -0800

    cleanup role message component

--- virtual-er/src/components/auth/roleMessage.tsx
+++ virtual-er/src/components/auth/roleMessage.tsx
@@ -1,25 +1,11 @@
-import { auth } from "@/auth";
 import { getRole } from "@/lib/actions";
 
 export default async function RoleMessage() {
     const role = await getRole();
-    let message = "";
-
-    if (role === "admin") {
-        message = "Signed in as Admin";
-    } else if (role === "patient") {
-        message = "Signed in as Patient";
-    } else if (role === "doctor") {
-        message = "Signed in as Doctor";
-    } else if (role === "nurse") {
-        message = "Signed in as Nurse";
-    } else if (role === "receptionist") {
-        message = "Signed in as Receptionist";
-    }
 
     return (
         <span className="text-sm text-gray-600">
-            {message}
+            <p>Signed in as <span className="capitalize">{role}</span></p>
         </span>
     )
 }
